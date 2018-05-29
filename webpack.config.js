d7ba95936fbff52262a99d4c69cd2b1f9d0299b0
848099532@qq.com <848099532@qq.com>
2018-05-29 14:41:17 +0800

    部署

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var path = require('path')
 // 在内存中，根据指定的模板页面，生成一份内存中的首页，同时自动把打包好的bundle注入到页面底部
 // 如果要配置插件，需要在导出的对象中，挂载一个 plugins 节点
-//var htmlWebpackPlugin = require('html-webpack-plugin')
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 // 当以命令行形式运行 webpack 或 webpack-dev-server 的时候，工具会发现，我们并没有提供 要打包 的文件的 入口 和 出口文件，此时，他会检查项目根目录中的配置文件，并读取这个文件，就拿到了导出的这个 配置对象，然后根据这个对象，进行打包构建
 module.exports = {
@@ -11,12 +11,12 @@ module.exports = {
     path: path.join(__dirname, './dist'), // 输出路径
     filename: 'bundle.js' // 指定输出文件的名称
   },
-//plugins: [ // 所有webpack  插件的配置节点
-//  new htmlWebpackPlugin({
-//    template: path.join(__dirname, './src/index.html'), // 指定模板文件路径
-//    filename: 'index.html' // 设置生成的内存页面的名称
-//  })
-//],
+plugins: [ // 所有webpack  插件的配置节点
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'), // 指定模板文件路径
+      filename: 'index.html' // 设置生成的内存页面的名称
+    })
+],
   module: { // 配置所有第三方loader 模块的
     rules: [ // 第三方模块的匹配规则
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }, // 处理 CSS 文件的 loader
